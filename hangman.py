28ce5bf8c0eaea4b217067201fe1592ddf06b159
Phinart98 <Phinart98@gmail.com>
2020-07-29 10:50:04 +0000

    updated the word library, made the code a bit more efficent and added a few comments

--- hangman.py
+++ hangman.py
@@ -2,21 +2,35 @@
 import random
 print("H A N G M A N")
 print()
-words = ['python', 'java', 'kotlin', 'javascript']
+# word library
+words = ["python", "java", "kotlin", "javascript", "algorithm", "apache", "ajax", "bootstrap", "codeigniter",
+         "coffeescript", "django", "flask", "framework", "git", "github", "swift", "jquery", "linux", "mongodb",
+         "mysql", "php", "postgresql", "bit", "bug", "byte", "code", "command", "computer", "computing", "pycharm",
+         "pytorch", "tensorflow", "machine", "data", "science", "function", "loop", "parameter", "programming",
+         "program", "variable", "react", "angular", "matrix", "facebook", "microsoft", "apple", "amazon", "netflix",
+         "google", "samsung", "dell", "sony", "ibm", "intel", "uber", "twitter", "snapchat", "tesla", "hyperloop"]
+
+# select word and hide from player
 correct_word = random.choice(words)
 display = len(correct_word) * '-'
 display_list = list(display)
 lives = 8
-entered_letters = []
+entered_letters = set()  # will contain letters that user enters.
 
+# continue asking user for these till word is guessed or lives are finished
 while lives != 0:
     print()
+    print("Guess the word below:")
+    print("Note: All words entered are supposed to be lowercase.")
     print(display)
+    print(f"You have {lives} lives left.")
     user_guess = input("Input a letter: ")
+
+    # life reduction exception cases
     if user_guess in entered_letters:
         print("You already typed this letter")
         continue
-    entered_letters.append(user_guess)
+    entered_letters.add(user_guess)
 
     if len(user_guess) != 1:
         print("You should input a single letter")
@@ -26,9 +40,11 @@ while lives != 0:
         print("It is not an ASCII lowercase letter")
         continue
 
+    # comparing user guesses to letters in computer choice
     if user_guess in correct_word:
         count = correct_word.count(user_guess)
 
+        # accounting for instances where the letter entered appears in the word multiple times instead of just once
         if count < 2:
             index = correct_word.find(user_guess)
             display_list[index] = user_guess
@@ -55,6 +71,7 @@ while lives != 0:
         lives -= 1
 
 else:
+    print(f"The correct word is: {correct_word}")
     print("You are hanged!")
 
 
